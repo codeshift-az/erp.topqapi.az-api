3a5c7dffc74c74ba4515ac9f6ee0e17b8ce1d824
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-02-27 09:44:04 +0100

    Category Serializer added

---
+++ server/apps/category/logic/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from server.apps.category.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Serializer definition for Category model."""
+
+    class Meta:
+        """Meta definition for CategorySerializer."""
+
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "updated_at",
+            "created_at",
+        )
+        read_only_fields = (
+            "id",
+            "updated_at",
+            "created_at",
+        )
