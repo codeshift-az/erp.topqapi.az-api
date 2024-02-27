f95af3c7c6b45d3a5f9ba402db876c4913542d62
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-02-27 09:47:18 +0100

    Category Filters added

---
+++ server/apps/category/logic/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters
+
+from server.apps.category.models import Category
+
+
+class CategoryFilter(filters.FilterSet):
+    """FilterSet class for Category model."""
+
+    name = filters.CharFilter(field_name="name", lookup_expr="icontains")
+
+    class Meta:
+        model = Category
+        fields = ["name"]
