15a9d055ba3c19c3b6f9d795cddd73883798f3f4
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-02-28 19:29:29 +0100

    Driver model added

---
+++ server/apps/worker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from server.apps.core.models import CoreModel
+
+
+class Driver(CoreModel):
+    """Model definition for Driver."""
+
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta(CoreModel.Meta):
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
+
+    def __str__(self):
+        """Unicode representation of Driver."""
+
+        return self.name
