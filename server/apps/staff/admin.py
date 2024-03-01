f1989f02711a85c306b9b59a84a411bd4aac1fb7
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-01 17:22:50 +0100

    Driver model added to Staff

--- server/apps/staff/admin.py
+++ server/apps/staff/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-from server.apps.staff.models import Worker
+from server.apps.staff.models import Driver, Worker
 
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
     """Admin class for Worker model."""
+
+    list_display = (
+        "name",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "updated_at",
+        "created_at",
+    )
+    search_fields = ("name",)
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    """Admin class for Driver model."""
 
     list_display = (
         "name",
--- server/apps/staff/models.py
+++ server/apps/staff/models.py
@@ -15,3 +15,17 @@ class Worker(CoreModel):
     def __str__(self):
         """Unicode representation of Worker."""
         return self.name
+
+
+class Driver(CoreModel):
+    """Model definition for Driver."""
+
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta(CoreModel.Meta):
+        verbose_name = "Driver"
+        verbose_name_plural = "Drivers"
+
+    def __str__(self):
+        """Unicode representation of Driver."""
+        return self.name
