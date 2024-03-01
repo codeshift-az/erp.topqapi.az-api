a77bf70c7895485e705410b289b45dec8d1f4d14
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-01 17:34:24 +0100

    Seller model added

--- server/apps/staff/admin.py
+++ server/apps/staff/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from server.apps.staff.models import Driver, Worker
+from server.apps.staff.models import Driver, Seller, Worker
 
 
-@admin.register(Worker)
-class WorkerAdmin(admin.ModelAdmin):
-    """Admin class for Worker model."""
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    """Admin class for Driver model."""
 
     list_display = (
         "name",
@@ -19,9 +19,31 @@ class WorkerAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-@admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
-    """Admin class for Driver model."""
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    """Admin class for Seller model."""
+
+    list_display = (
+        "name",
+        "branch",
+        "salary",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "branch",
+        "updated_at",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "branch",
+    )
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    """Admin class for Worker model."""
 
     list_display = (
         "name",
--- server/apps/staff/models.py
+++ server/apps/staff/models.py
@@ -3,20 +3,6 @@ from django.db import models
 from server.apps.core.models import CoreModel
 
 
-class Worker(CoreModel):
-    """Model definition for Worker."""
-
-    name = models.CharField(max_length=255, unique=True)
-
-    class Meta(CoreModel.Meta):
-        verbose_name = "Worker"
-        verbose_name_plural = "Workers"
-
-    def __str__(self):
-        """Unicode representation of Worker."""
-        return self.name
-
-
 class Driver(CoreModel):
     """Model definition for Driver."""
 
@@ -28,4 +14,34 @@ class Driver(CoreModel):
 
     def __str__(self):
         """Unicode representation of Driver."""
+        return self.name
+
+
+class Seller(CoreModel):
+    """Model definition for Seller."""
+
+    name = models.CharField(max_length=255, unique=True)
+    branch = models.ForeignKey("branch.Branch", on_delete=models.CASCADE, related_name="sellers")
+    salary = models.PositiveIntegerField(default=0)
+
+    class Meta(CoreModel.Meta):
+        verbose_name = "Seller"
+        verbose_name_plural = "Sellers"
+
+    def __str__(self):
+        """Unicode representation of Seller."""
+        return f"{self.name} - {self.branch.name}"
+
+
+class Worker(CoreModel):
+    """Model definition for Worker."""
+
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta(CoreModel.Meta):
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+    def __str__(self):
+        """Unicode representation of Worker."""
         return self.name
