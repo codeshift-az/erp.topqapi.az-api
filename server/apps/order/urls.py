d1c10cc7b9d575bf44b8b535aabcd44cfd83d0ae
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-14 20:42:25 +0100

    Order Item Viewset and Endpoints added

--- server/apps/order/urls.py
+++ server/apps/order/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import SimpleRouter
 
-from server.apps.order.views import OrderViewSet
+from server.apps.order.views import OrderItemViewSet, OrderViewSet
 
 app_name = "orders"
 
 router = SimpleRouter(trailing_slash=True)
-router.register(app_name, OrderViewSet)
+router.register(f"{app_name}", OrderViewSet)
+router.register(f"{app_name}/items", OrderItemViewSet)
 
 urlpatterns = router.urls
--- server/apps/order/views.py
+++ server/apps/order/views.py
@@ -5,9 +5,9 @@ from rest_framework import status, viewsets
 from server.apps.core.logic import responses
 
 # Order
-from server.apps.order.logic.filters import OrderFilter
-from server.apps.order.logic.serializers import OrderSerializer
-from server.apps.order.models import Order
+from server.apps.order.logic.filters import OrderFilter, OrderItemFilter
+from server.apps.order.logic.serializers import OrderItemSerializer, OrderSerializer
+from server.apps.order.models import Order, OrderItem
 
 # User Types
 from server.apps.user.models import UserTypes
@@ -41,6 +41,101 @@ class OrderViewSet(viewsets.ModelViewSet):
 
         if self.request.user.type == UserTypes.STORE:
             return queryset.filter(branch=self.request.user.branch)
+
+        return queryset.all()
+
+    @extend_schema(
+        description=f"Retrieve list of all {verbose_name_plural}.",
+        responses={
+            status.HTTP_200_OK: serializer_class,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+        },
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        description=f"Retrieve a {verbose_name} by {lookup_field}.",
+        responses={
+            status.HTTP_200_OK: serializer_class,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+            status.HTTP_404_NOT_FOUND: responses.NOT_FOUND,
+        },
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        description=f"Create a new {verbose_name}.",
+        responses={
+            status.HTTP_201_CREATED: serializer_class,
+            status.HTTP_400_BAD_REQUEST: responses.BAD_REQUEST,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+        },
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        description=f"Update an existing {verbose_name} by {lookup_field}.",
+        responses={
+            status.HTTP_200_OK: serializer_class,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+            status.HTTP_404_NOT_FOUND: responses.NOT_FOUND,
+        },
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        description=f"Partially update an existing {verbose_name} by {lookup_field}.",
+        responses={
+            status.HTTP_200_OK: serializer_class,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+            status.HTTP_404_NOT_FOUND: responses.NOT_FOUND,
+        },
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        description=f"Delete an existing {verbose_name} by {lookup_field}.",
+        responses={
+            status.HTTP_204_NO_CONTENT: None,
+            status.HTTP_401_UNAUTHORIZED: responses.UNAUTHORIZED,
+            status.HTTP_403_FORBIDDEN: responses.FORBIDDEN,
+        },
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    """Viewset for Order model."""
+
+    queryset = OrderItem.objects.none()
+    serializer_class = OrderItemSerializer
+
+    filterset_class = OrderItemFilter
+    search_fields = None
+    ordering_fields = "__all__"
+
+    verbose_name = "item in orders"
+    verbose_name_plural = "items in orders"
+
+    lookup_field = "id"
+
+    def get_queryset(self):
+        queryset = OrderItem.objects.select_related(
+            "product",
+            "product__category",
+            "supplier",
+        )
 
         return queryset.all()
 
