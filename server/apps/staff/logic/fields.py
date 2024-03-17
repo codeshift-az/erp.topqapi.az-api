857be28fe2c65824650e93d69d6ae6956c3cd209
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-17 21:20:19 +0100

    Staff Driver and Worker fields added

--- server/apps/staff/logic/fields.py
+++ server/apps/staff/logic/fields.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
 
 import server.apps.staff.logic.schema  # noqa: F401
-from server.apps.staff.logic.serializers import SellerSerializer
-from server.apps.staff.models import Seller
+from server.apps.staff.logic.serializers import DriverSerializer, SellerSerializer, WorkerSerializer
+from server.apps.staff.models import Driver, Seller, Worker
+
+
+class DriverField(serializers.RelatedField):
+    """Serializer definition for Driver field."""
+
+    def get_queryset(self):
+        """Return queryset of Driver objects."""
+        return Driver.objects.all()
+
+    def to_representation(self, value: Driver) -> dict:
+        """Return representation of Driver object."""
+        return DriverSerializer(value).data
+
+    def to_internal_value(self, data: int) -> Driver:
+        """Return Driver object from given data."""
+        driver = Driver.objects.get(pk=data)
+
+        if driver is None:
+            return serializers.ValidationError(f"Driver with id {data} does not exist.")
+
+        return driver
 
 
 class SellerField(serializers.RelatedField):
@@ -24,3 +45,24 @@ class SellerField(serializers.RelatedField):
             return serializers.ValidationError(f"Seller with id {data} does not exist.")
 
         return seller
+
+
+class WorkerField(serializers.RelatedField):
+    """Serializer definition for Worker field."""
+
+    def get_queryset(self):
+        """Return queryset of Worker objects."""
+        return Worker.objects.all()
+
+    def to_representation(self, value: Worker) -> dict:
+        """Return representation of Worker object."""
+        return WorkerSerializer(value).data
+
+    def to_internal_value(self, data: int) -> Worker:
+        """Return Worker object from given data."""
+        worker = Worker.objects.get(pk=data)
+
+        if worker is None:
+            return serializers.ValidationError(f"Worker with id {data} does not exist.")
+
+        return worker
--- server/apps/staff/logic/schema.py
+++ server/apps/staff/logic/schema.py
@@ -4,7 +4,17 @@ from drf_spectacular.plumbing import build_basic_type
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import Direction
 
-from server.apps.staff.logic.serializers import SellerSerializer
+from server.apps.staff.logic.serializers import DriverSerializer, SellerSerializer, WorkerSerializer
+
+
+class DriverFieldSchema(OpenApiSerializerFieldExtension):
+    target_class = "server.apps.staff.logic.fields.DriverField"
+
+    def map_serializer_field(self, auto_schema: AutoSchema, direction: Direction):
+        if direction == "request":
+            return build_basic_type(OpenApiTypes.INT)
+
+        return auto_schema.resolve_serializer(DriverSerializer, direction).ref
 
 
 class SellerFieldSchema(OpenApiSerializerFieldExtension):
@@ -15,3 +25,13 @@ class SellerFieldSchema(OpenApiSerializerFieldExtension):
             return build_basic_type(OpenApiTypes.INT)
 
         return auto_schema.resolve_serializer(SellerSerializer, direction).ref
+
+
+class WorkerFieldSchema(OpenApiSerializerFieldExtension):
+    target_class = "server.apps.staff.logic.fields.WorkerField"
+
+    def map_serializer_field(self, auto_schema: AutoSchema, direction: Direction):
+        if direction == "request":
+            return build_basic_type(OpenApiTypes.INT)
+
+        return auto_schema.resolve_serializer(WorkerSerializer, direction).ref
