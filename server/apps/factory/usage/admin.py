6878ded92cabab5b0ebcbfbd198a8ee048a1a18f
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-23 14:51:46 +0400

    Factory Usage Models added

---
+++ server/apps/factory/usage/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+
+from server.apps.factory.usage.models import FactoryUsage, FactoryUsageItem
+
+
+@admin.register(FactoryUsage)
+class FactoryUsageAdmin(admin.ModelAdmin):
+    """Admin class for FactoryUsage model."""
+
+    list_display = (
+        "product",
+        "quantity",
+        "price",
+        "date",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "product",
+        "price",
+        "date",
+        "updated_at",
+        "created_at",
+    )
+    search_fields = ("product",)
+
+
+@admin.register(FactoryUsageItem)
+class FactoryUsageItemAdmin(admin.ModelAdmin):
+    """Admin class for FactoryUsageItem model."""
+
+    list_display = (
+        "usage",
+        "storage",
+        "quantity",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "updated_at",
+        "created_at",
+    )
