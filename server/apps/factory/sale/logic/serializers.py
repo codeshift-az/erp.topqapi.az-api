6e17ec3886f2504c70a28c8d9168a21892227271
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-03-23 15:36:30 +0400

    Factory Sale Update added to serializer

--- server/apps/factory/sale/logic/serializers.py
+++ server/apps/factory/sale/logic/serializers.py
@@ -40,7 +40,44 @@ class FactorySaleSerializer(serializers.ModelSerializer):
 
         while count > 0:
             sale.items.create(
-                sale=self.instance,
+                sale=sale,
+                storage=items[n],
+                quantity=count if items[n].left >= count else items[n].left,
+            )
+            count -= items[n].left
+            n += 1
+
+        return sale
+
+    def update(self, instance, validated_data):
+        quantity = validated_data.get("quantity", None)
+        product = validated_data.get("product", None)
+
+        items = (
+            FactoryStorageItem.objects.get_related()
+            .get_sales()
+            .filter(product=product if product else instance.product, left__gt=0)
+        )
+
+        if quantity and (sum([item.left for item in items]) + (instance.quantity if product else 0) < quantity):
+            raise serializers.ValidationError({"quantity": "Istehsalat anbarında bu məhsuldan yetərli qədər yoxdur!"})
+
+        sale = super().update(instance, validated_data)
+
+        if quantity is None and product is None:
+            return sale
+
+        for item in sale.items.all():
+            item.delete()
+
+        count = sale.quantity
+        n = 0
+
+        items = list(FactoryStorageItem.objects.get_related().get_sales().filter(product=sale.product, left__gt=0))
+
+        while count > 0:
+            sale.items.create(
+                sale=sale,
                 storage=items[n],
                 quantity=count if items[n].left >= count else items[n].left,
             )
